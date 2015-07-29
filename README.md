a47cde2fcb3511a1d51c559d20ebf61756ed2439
benatespina <benatespina@gmail.com>
2015-07-29 15:04:14 +0200

    Improvements to publis the plugin

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
-Template selector
-=================
+# Template selector
+> Adds a field to declare templates used by pages dynamically
 
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/LIN3S/Distribution/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/LIN3S/Distribution/?branch=master)
+[![Total Downloads](https://poser.pugx.org/lin3s/lin3s-distribution/downloads)](https://packagist.org/packages/lin3s/lin3s-distribution)
+&nbsp;&nbsp;&nbsp;&nbsp;
+[![Latest Stable Version](https://poser.pugx.org/lin3s/lin3s-distribution/v/stable.svg)](https://packagist.org/packages/lin3s/lin3s-distribution)
+[![Latest Unstable Version](https://poser.pugx.org/lin3s/lin3s-distribution/v/unstable.svg)](https://packagist.org/packages/lin3s/lin3s-distribution)
+
+## Why?
+[LIN3S][1]'s [WPRouting][2] is a very robust solution to manage the Wordpress routing system in a [Symfony][3] way.
+However, it has a 
 Adds a simple input to type the template you want to use for each page. Useful when working with wp-routing.
 
-Usage
------
+## Installation
+The recommended and the most suitable way to install is through [Composer][4]. Be sure that the tool is installed
+in your system and copy the following piece of code inside your `composer.json`:
+```
+{
+    "type": "composer",
+    "url": "http://wpackagist.org"
+},
+"require": {
+    "wpackagist-plugin/template-selector": "~1.0"
+}
+```
 
+## Usage
 To declare the templates you want to use just add the following hook to your WordPress theme.
 
     add_filter('template_selector_available', 'addAvailableTemplates');
@@ -16,4 +36,11 @@ To declare the templates you want to use just add the following hook to your Wor
             "another-template" => 'Another template'
         ]);
     }
-    
+
+## Licensing Options
+[![License](https://poser.pugx.org/lin3s/lin3s-distribution/license.svg)](https://github.com/LIN3S/Distribution/blob/master/LICENSE)
+
+[1]: http://lin3s.com
+[2]: 
+[3]: 
+[4]: https://getcomposer.org/
--- template-selector.php
+++ template-selector.php
@@ -1,44 +1,58 @@
 <?php
+
 /*
-Plugin Name: Template selector
-Plugin URI: http://lin3s.com
-Description: Adds a field to declare templates used by pages dynamically
-Author: Gorka Laucirica
-Version: 0.1
-Author URI: http://lin3s.com/
-*/
+ * Plugin Name: Template selector
+ * Plugin URI: http://lin3s.com
+ * Description: Adds a field to declare templates used by pages dynamically.
+ * Author: LIN3S
+ * Version: 1.0
+ * Author URI: http://lin3s.com/
+ */
 
 new TemplateSelector();
 
+/**
+ * Plugin template selector class.
+ *
+ * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
+ */
 class TemplateSelector
 {
     const META_STRING = '_wp_routing_template';
     const TRANSLATION_DOMAIN = 'wp_routing';
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
-        add_action('add_meta_boxes', array($this, 'addMetabox'));
-        add_action('save_post', array($this, 'saveSelectedTemplate'));
+        add_action('add_meta_boxes', [$this, 'addMetabox']);
+        add_action('save_post', [$this, 'saveSelectedTemplate']);
     }
 
+    /**
+     * Adds the metabox calling the Wordpress' internal "add_meta_box" method.
+     */
     public function addMetabox()
     {
-        add_meta_box('wp_routing_template',
+        add_meta_box(
+            'wp_routing_template',
             __('Template selector', self::TRANSLATION_DOMAIN),
-            array($this, 'renderTemplateSelector'),
+            [$this, 'renderTemplateSelector'],
             'page',
             'side'
         );
     }
 
+    /**
+     * Renders the template selector. Also, added
+     * a nonce field so to check it later.
+     */
     public function renderTemplateSelector()
     {
         global $post;
 
-        $templates = apply_filters('template_selector_available', array(
-            "default" => "Default"
-        ));
-        // Add a nonce field so we can check for it later.
+        $templates = apply_filters('template_selector_available', ['default' => 'Default']);
         wp_nonce_field(self::META_STRING, self::META_STRING . '_nonce');
 
         $current = get_post_meta($post->ID, '_wp_page_template', true);
@@ -48,14 +62,21 @@ class TemplateSelector
         echo '</label></p>';
         echo '<select id="' . self::META_STRING . '" name="' . self::META_STRING . '">';
         foreach ($templates as $slug => $name) {
-            echo '<option value="'. $slug .'" ' . ($current == $slug ? 'selected' : '') . '>' . $name . '</option>';
+            echo '<option value="' . $slug . '" ' . ($current == $slug ? 'selected' : '') . '>' . $name . '</option>';
         }
         echo '<select>';
     }
 
-    public function saveSelectedTemplate($post_id)
+    /**
+     * Saves the selected template of post id given.
+     *
+     * This method also checks if the nonce is set and
+     * updated the meta field in the database.
+     *
+     * @param int $postId The post id
+     */
+    public function saveSelectedTemplate($postId)
     {
-        // Check if our nonce is set.
         if (!isset($_POST[self::META_STRING . '_nonce']) ||
             !wp_verify_nonce($_POST[self::META_STRING . '_nonce'], self::META_STRING) ||
             (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) ||
@@ -64,7 +85,6 @@ class TemplateSelector
             return;
         }
 
-        // Update the meta field in the database.
-        update_post_meta($post_id, '_wp_page_template', sanitize_text_field($_POST[self::META_STRING]));
+        update_post_meta($postId, '_wp_page_template', sanitize_text_field($_POST[self::META_STRING]));
     }
 }
